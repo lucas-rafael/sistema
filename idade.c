f9e5800c4aaa4f8ce3230f2cd118decb29a09d89
Alan <alan.silva@jaime.com.br>
2015-12-15 11:08:50 -0200

    Segundo commit

---
+++ idade.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* idade.c*/
+/*programa para calcular a sua idade em dias*/
+main()
+{
+	float anos, dias; /*variaveis*/
+	printf("Digite valor de v: ");
+	scanf("%f"); /*ler idade em anos*/
+	v = v *365; /*calcula a idade em dias*/
+	printf("Sua v em m e' %.0f \n", m); /*impressao do resultado*/
+	system("pause");
+}
