ef02934d47663cee9aef6fa31f37a78fde42e515
Lucas Rafael <lopes0073@hotmail.com>
2017-08-17 11:29:48 -0300

    Update idade.c

--- idade.c
+++ idade.c
@@ -5,9 +5,9 @@
 main()
 {
 	float anos, dias; /*variaveis*/
-	printf("Digite valor de v: ");
+	printf("Digite sia idade em anos: ");
 	scanf("%f"); /*ler idade em anos*/
 	v = v *365; /*calcula a idade em dias*/
-	printf("Sua v em m e' %.0f \n", m); /*impressao do resultado*/
+	printf("Sua idade em dias e' %.0f \n", m); /*impressao do resultado*/
 	system("pause");
 }
